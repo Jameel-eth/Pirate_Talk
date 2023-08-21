055f95a5034433e5becb20b56e7106fe78e8dde2
Jameel-eth <126181916+Jameel-eth@users.noreply.github.com>
2023-08-21 16:23:41 -0400

    Initial Upload

---
+++ PirateTalk.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>How To Talk Like a Pirate</title>
+</head>
+<body>
+	<h1>How to Talk Like a Pirate!</h1>
+	<?php
+		echo "<p>Avast me hearties! Return handsomley with some fine swag, ye scurvy dogs!''</p> <p> Else, we be keelhaulin' ye next morn...</p> "
+	?>
+	<h2>You're Ready to be a Pirate!</h2>
+</body>
+</html>
